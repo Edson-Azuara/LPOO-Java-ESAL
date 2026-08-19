7b4e43660961f5e69ebee64e533181ec00432bce
Edson Azuara <edazuaral007@gmail.com>
2026-08-19 11:20:14 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+//Práctica #1
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(suma(3, 5));
+    }
+
+    public static int suma(int a, int i) {
+        return a + i;
+    }
+}
